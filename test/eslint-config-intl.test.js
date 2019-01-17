5869e80b9a181d64fae649836b502bb898960682
Yuheng Zhang <zhangyuheng91@gmail.com>
2019-01-17 20:18:42 +0800

    test: fix assertion

--- test/eslint-config-intl.test.js
+++ test/eslint-config-intl.test.js
@@ -3,7 +3,6 @@ import core from '..'
 
 test('default', t => {
   t.true(typeof core.root === 'boolean')
-  t.true(typeof core.parser === 'string')
   t.true(typeof core.parserOptions === 'object')
   t.true(typeof core.env === 'object')
   t.true(typeof core.rules === 'object')
